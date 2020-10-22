3914ed10e444773c748c3a27ba20fef07b181af9
xiaokang <13148807065@163.com>
2020-10-23 00:53:39 +0800

    部署

--- src/main/java/com/hbhb/cw/messagehub/Application.java
+++ src/main/java/com/hbhb/cw/messagehub/Application.java
@@ -3,7 +3,6 @@ package com.hbhb.cw.messagehub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -18,7 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 @SpringCloudApplication
 @ComponentScan("com.hbhb")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
